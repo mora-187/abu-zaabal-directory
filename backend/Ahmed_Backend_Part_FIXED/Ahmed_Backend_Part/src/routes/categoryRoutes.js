772d89b873bb630b65bc595673b3123f39ad9cc2
Ahmed_Farouh <farouhahmed62@gmail.com>
2026-08-29 23:46:21 +0300

    Delete backend/Ahmed_Backend_Part_FIXED/Ahmed_Backend_Part directory

--- backend/Ahmed_Backend_Part_FIXED/Ahmed_Backend_Part/src/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getGroups, getCategories } = require('../controllers/categoryController');
-
-const router = express.Router();
-
-router.get('/groups', getGroups);
-router.get('/categories', getCategories);
-
-module.exports = router;
